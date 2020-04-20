dac09c4980d7ed246f1f8f6ff2d55ce55be9dfb8
Thadeu Cesário <thadeu.cesario@icloud.com>
2020-04-19 21:42:32 -0300

    eslint

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -1,10 +1,8 @@
-import express, { response } from 'express';
+import express from 'express';
 
 const app = express();
 
-app.get('/', (request, response) => {
-  response.json({message: "Hello TypeScript Project!!!!"})
-});
+app.get('/', (request, response) => response.json({ message: 'Hello TypeScript Project!!!!' }));
 
 app.listen(3333, () => {
   console.log('👾️ Backend server started! 👾️');
