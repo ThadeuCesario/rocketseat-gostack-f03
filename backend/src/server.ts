51b2a6597182b89cfd82252cbc3478e56c8eabc2
Thadeu Cesário <thadeu.cesario@icloud.com>
2020-04-21 13:53:07 -0300

    eslint

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -2,9 +2,7 @@ import express from 'express';
 
 const app = express();
 
-app.get('/', (request, response) => {
-  return response.json({message: 'Hello Thadeu'});
-});
+app.get('/', (request, response) => response.json({ message: 'Hello Hello Hello Hello hELLO' }));
 
 app.listen(3333, () => {
   console.log('Backend start on port 3333');
