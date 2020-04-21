bf49f0a19ea13a115b933c8cee653a7ae95c1448
Thadeu Cesário <thadeu.cesario@icloud.com>
2020-04-21 14:28:16 -0300

    Prettier + ESLint

--- backend/src/routes/index.ts
+++ backend/src/routes/index.ts
@@ -2,6 +2,8 @@ import { Router } from 'express';
 
 const routes = Router();
 
-routes.get('/', (request, response) => response.json({ message: 'Hello Hello Hello Hello hELLO' }));
+routes.get('/', (request, response) =>
+  response.json({ message: 'Hello Hello Hello Hello hELLO' })
+);
 
 export default routes;
