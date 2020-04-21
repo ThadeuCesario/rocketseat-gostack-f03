8d22bbc997a965f8cfa0b6c89bb2fd0711e6bf82
Thadeu Cesário <thadeu.cesario@icloud.com>
2020-04-21 14:18:04 -0300

    import ts

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -1,8 +1,9 @@
 import express from 'express';
+import routes from './routes';
 
 const app = express();
 
-app.get('/', (request, response) => response.json({ message: 'Hello Hello Hello Hello hELLO' }));
+app.use(routes);
 
 app.listen(3333, () => {
   console.log('Backend start on port 3333');
