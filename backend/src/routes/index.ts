e8c1e95c469a59ac8f2119e8831f7f92250ece6c
Thadeu Cesário <thadeu.cesario@icloud.com>
2020-04-25 18:36:06 -0300

    Testing debug

--- backend/src/routes/index.ts
+++ backend/src/routes/index.ts
@@ -8,10 +8,15 @@ import { Router } from 'express';
 
 const routes = Router();
 
-routes.get('/', (request, response) =>
-    response.json({
-        message: 'Hello GoStaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaack',
-    }),
-);
+routes.post('/users', (request, response) => {
+    const { name, email } = request.body;
+
+    const user = {
+        name,
+        email,
+    };
+
+    return response.json(user);
+});
 
 export default routes;
