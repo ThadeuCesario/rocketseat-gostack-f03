eff9a89c05620c0a58ef1c04ccf7213e5f457812
Thadeu Cesário <thadeu.cesario@icloud.com>
2020-04-25 16:53:41 -0300

    Config ts-node-dev GoBarber Project

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -1,7 +1,21 @@
+/**
+ * Estamos utilizando o 'ts-node-dev' no projeto. Estamos executando um projeto
+ * com TypeScript e Node. O 'ts-node-dev' tem um bom funcionamento e fornece
+ * uma ótima experiência de desenvolvimento.
+ * Eu criei dentro do package.json um script para executar o 'ts-node-dev'.
+ * Passei duas propriedades para o script também, a primeira é o transpileOnly que
+ * passará a não verificar se há erros em nosso código. E a segunda é a ignore-watch node_modules
+ * para que a pasta node_modules seja ignorada.
+ */
+
 import express from 'express';
 
 const app = express();
 
-app.listen(3333, ()=>{
+app.get('/', (request, response) => {
+  return response.json({message: 'Hello GoStack'});
+})
+
+app.listen(3333, () => {
   console.log('Server back-end started!')
 });
