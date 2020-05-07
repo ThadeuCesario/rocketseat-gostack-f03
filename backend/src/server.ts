01b5131e6bd0966006a93b3c53c1d07588a756f8
Thadeu Cesário <thadeu.cesario@icloud.com>
2020-05-06 21:38:22 -0300

    Setting global exception handler

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -29,16 +29,52 @@
  */
 
 import 'reflect-metadata';
-import express from 'express';
+import express, { Request, Response, NextFunction } from 'express';
+import 'express-async-errors';
 import routes from './routes';
 import uploadConfig from './config/upload';
 import './database';
+import AppError from './errors/AppError';
 
 const app = express();
 
 app.use(express.json());
 app.use('/files', express.static(uploadConfig.directory));
 app.use(routes);
+
+/*
+ *O erro acontecerá nas rotas, portanto precisamos realizar a trativas dos errors
+ * depois das rotas.
+ *
+ * Os middlewares para tratativa de erros do express, são obrigados a terem
+ * quatro parâmetros. Que são:
+ * - error
+ * - request
+ * - response
+ * - next
+ */
+
+app.use(
+    (error: Error, request: Request, response: Response, _: NextFunction) => {
+        if (error instanceof AppError) {
+            /**
+             * Se true, é um erro gerado pela minha aplicação, portanto é um erro
+             * conhecido.
+             */
+            return response.status(error.statusCode).json({
+                status: 'error',
+                message: error.message,
+            });
+        }
+
+        console.error(error);
+
+        return response.status(500).json({
+            status: 'error',
+            message: 'internal server error',
+        });
+    },
+);
 
 app.listen(3333, () => {
     console.log('Server back-end started!');
